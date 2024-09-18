6ba73399da71bb7fea13a8ee32c0e64096413475
Jarosław Zwierz <jaroslaw.zwierz@student.po.edu.pl>
2024-09-18 17:05:04 +0200

    fixed problem with multiple connection attempts

--- client/components/utility/connection/socket.ts
+++ client/components/utility/connection/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:4000", {
+  reconnection: false,
   reconnectionDelayMax: 10000,
   auth: {
     token: "123"
--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,7 @@
 const { Server } = require("socket.io");
 
+let connectedClients = [];
+
 function configureSocket() { // Socket.io configuration
   const io = new Server(4000, {
     cors: {
@@ -11,6 +13,15 @@ function configureSocket() { // Socket.io configuration
   });
 
   io.on("connection", (socket) => {
+    const clientAddress = socket.handshake.address;
+
+    if (connectedClients.includes(clientAddress)) {
+      console.log("Client already connected!");
+      socket.disconnect();
+      return;
+    }
+
+    connectedClients.push(clientAddress);
     console.log("A user connected!");
 
     socket.on("message", (message, callback) => {
@@ -20,6 +31,7 @@ function configureSocket() { // Socket.io configuration
 
     socket.on("disconnect", () => {
       console.log("user disconnected");
+      connectedClients = connectedClients.filter(address => address !== clientAddress);
     });
   });
 };
